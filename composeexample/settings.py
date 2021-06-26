5a93bf517cedaad51b5e8eb4adc0bd3481d5a36f
YaCpotato <yasshisshy@gmail.com>
2021-06-26 23:25:18 +0900

    フロントとの接続成功

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-6k@ltu7=gp6m40@p8_h4^$ix_b4b=1$xj+^98=atb_v9760a8h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8080'
+]
 
 # Application definition
 
@@ -39,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api'
 ]
 
@@ -50,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'composeexample.urls'
